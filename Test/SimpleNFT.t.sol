960a055a3df8b9ff3cc355c0fe9662798b60b5a9
unknown <alexandre.rafales@gmail.com>
2025-01-29 19:47:38 +0100

    foundry first tests

--- Test/SimpleNFT.t.sol
+++ Test/SimpleNFT.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, Vm} from "forge-std/Test.sol"; // Ajout de Vm pour makeAddr()
 import "../Contracts/SimpleNFT.sol";
 import {console} from "forge-std/console.sol"; // Nécessaire pour `console.log`
 
@@ -10,29 +10,29 @@ contract SimpleNFTTest is Test {
     address private owner;
     address private addr1;
     address private addr2;
-   event Debug(string message, address addr, uint256 tokenId);
+    event Debug(string message, address addr, uint256 tokenId);
     address private constant ZERO_ADDRESS = address(0);
 
     function setUp() public {
         owner = address(this);
-        addr2 = makeAddr("addr2");
+    addr1 = vm.addr(1);
+    addr2 = vm.addr(2);
 
         simpleNFT = new SimpleNFT("TestNFT", "TNFT");
     }
 
-    function testInitialNameAndSymbol() public view {
-        assertEq(simpleNFT.name(), "TestNFT");
-        assertEq(simpleNFT.symbol(), "TNFT");
-    }
-
-function testMintAndOwnership() public {
-    simpleNFT.mint(addr1, 1);
-
-    assertEq(simpleNFT.balanceOf(addr1), 1);
-    assertEq(simpleNFT.ownerOf(1), addr1);
+    function testInitialNameAndSymbol() public {
+    assertEq(simpleNFT.name(), "TestNFT");
+    assertEq(simpleNFT.symbol(), "TNFT");
 }
 
 
+    function testMintAndOwnership() public {
+        simpleNFT.mint(addr1, 1);
+
+        assertEq(simpleNFT.balanceOf(addr1), 1);
+        assertEq(simpleNFT.ownerOf(1), addr1);
+    }
 
     function testCannotMintToZeroAddress() public {
         vm.expectRevert(
@@ -72,47 +72,28 @@ function testMintAndOwnership() public {
         );
         simpleNFT.transferFrom(addr1, addr2, 1);
     }
-function testApproveAndGetApproval() public {
-    // Mint un token pour addr1
-    simpleNFT.mint(addr1, 1);
-    console.log("Mint complete: Owner =", simpleNFT.ownerOf(1));
 
-    // addr1 approuve addr2
+function testApproveAndGetApproval() public {
+    simpleNFT.mint(addr1, 1);
     vm.prank(addr1);
     simpleNFT.approve(addr2, 1);
-    console.log("Approved addr2: Approved =", simpleNFT.getApproved(1));
+    
     assertEq(simpleNFT.getApproved(1), addr2);
 
-    // Effacer l'approbation
     vm.prank(addr1);
-    simpleNFT.approve(address(0), 1);
-    console.log("Approval cleared: Approved =", simpleNFT.getApproved(1));
-    assertEq(simpleNFT.getApproved(1), address(0));
+vm.expectRevert(abi.encodeWithSelector(SimpleNFT.InvalidRecipient.selector, address(0)));
+simpleNFT.approve(address(0), 1);
+
 }
-
-
-
-
-
-
-
-
-
 
 
 function testCannotApproveZeroAddress() public {
     simpleNFT.mint(addr1, 1);
 
-    // Approving address(0) to clear should not revert
     vm.prank(addr1);
-    simpleNFT.approve(address(0), 1);
-
-    // Approving address(0) as the recipient should revert
-    vm.expectRevert(SimpleNFT.InvalidRecipient.selector);
-    simpleNFT.approve(address(0), 1);
+    vm.expectRevert(abi.encodeWithSelector(SimpleNFT.InvalidRecipient.selector, address(0)));
+simpleNFT.approve(address(0), 1);
 }
-
-
 
     function testSetApprovalForAllAndTransfer() public {
         simpleNFT.mint(addr1, 1);
